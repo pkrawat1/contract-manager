22b0b61074c645bdb0d7ee4d0273f3699e6619f0
Pankaj Rawat <pankajrawat19sept@gmail.com>
2018-05-08 21:07:25 +0530

    fixing otp app name

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -12,7 +12,7 @@ config :contract_manager, ContractManagerWeb.Endpoint,
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Configure your database
-config :takeaplege_api, TakeaplegeApi.Repo,
+config :contract_manager, ContractManagerWeb.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
