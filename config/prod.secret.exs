a6ee3d11c2f8925fff9fb276ff0a0896ba0495f4
Pankaj Rawat <pankajrawat19sept@gmail.com>
2018-05-08 21:13:04 +0530

    prod config fixes

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -1,15 +1,14 @@
 use Mix.Config
 
-# In this file, we keep production configuration that
-# you'll likely want to automate and keep away from
-# your version control system.
-#
-# You should document the content of this
-# file or create a script for recreating it, since it's
-# kept out of version control and might be hard to recover
-# or recreate for your teammates (or yourself later on).
 config :contract_manager, ContractManagerWeb.Endpoint,
+  load_from_system_env: true,
+  url: [scheme: "https", host: "ct-man.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+# Do not print debug messages in production
+config :logger, level: :info
 
 # Configure your database
 config :contract_manager, ContractManagerWeb.Repo,
